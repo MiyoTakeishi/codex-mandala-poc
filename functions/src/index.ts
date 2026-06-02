a75b8fd75f58038cc9073a853cdd40d0a3510c9d
MiyoTakeishi <ennead00000012@gmail.com>
2026-06-02 16:48:43 +0900

    feat: add chart deletion function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 export { addEditor } from "./callable/addEditor";
 export { createChart } from "./callable/createChart";
 export { createInviteLink } from "./callable/createInviteLink";
+export { deleteChart } from "./callable/deleteChart";
 export { removeEditor } from "./callable/removeEditor";
 export { verifyEditorAccess } from "./callable/verifyEditorAccess";
